1c2810eb503e12d7e13d0c5431b956547d0b2a22
Elan Majkrzak <elanmajkrzak@gmail.com>
2020-03-13 21:04:51 -0700

    Move to heroku nostrum

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Nookinc.MixProject do
 
   defp deps do
     [
-      {:nostrum, "~> 0.4"},
+      {:nostrum_heroku, "~> 0.4.2"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"}
     ]
