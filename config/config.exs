47642d45056de21f9b79d3ac69a03af5653ce594
Elan Majkrzak <elanmajkrzak@gmail.com>
2020-03-14 15:05:15 -0700

    Update ecto repo config for gigalixir

--- config/config.exs
+++ config/config.exs
@@ -2,16 +2,15 @@ import Config
 
 config :nostrum,
   # The token of your bot as a string
-  token: System.get_env("BOT_TOKEN"),
+  token: System.get_env("BOT_TOKEN") || "NjgyNDMxMzk3NTI4NjAwNjM5.Xlnd5A.VZ5Lk4NtZYdvrEyx_yeqfnXP34w",
   # The number of shards you want to run your bot under, or :auto.
   num_shards: :auto
 
 config :nookinc,
   ecto_repos: [Nookinc.Repo]
 
-config :nookinc, Nookinc.Repo,
-  database: "nookinc",
-  username: System.get_env("DB_USER"),
-  password: System.get_env("DB_PASS"),
-  hostname: System.get_env("DB_HOST"),
-  port: System.get_env("DB_PORT")
+  config :nookinc, Nookinc.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    url: System.get_env("DATABASE_URL") || "ecto://postgres:darksteel@localhost:5433/nookinc",
+    ssl: false,
+    pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
--- config/prod.exs
+++ config/prod.exs
@@ -12,5 +12,5 @@ config :nookinc,
 config :nookinc, Nookinc.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  ssl: true,
+  ssl: false,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
