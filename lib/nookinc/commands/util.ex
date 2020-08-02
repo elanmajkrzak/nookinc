ceddea9d4aeb39fce1d891a83cab99f7c2f7529c
Elan Majkrzak <elanmajkrzak@gmail.com>
2020-08-01 18:42:45 -0700

    Updating help command for dream addresses

--- lib/nookinc/commands/util.ex
+++ lib/nookinc/commands/util.ex
@@ -8,6 +8,13 @@ defmodule Nookinc.Commands.Util do
       Ex. !fc @CheeseWeens
     **!fcadd <your friend code>**
       Stores your friend code so other users can look you up.
+    **!da**
+      Shows your own stored dream address.
+    **!da <@mention> <@mention> ...**
+      Shows the mentioned users stored dream addresses, if they have stored it.
+      Ex. !da @CheeseWeens
+    **!daadd <your dream address>**
+      Stores your dream address so anybody can come check your island out!
     """
   end
 end
